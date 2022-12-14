738bd54283d431b7a437e3577307773174dd70bf
Maliviwe Diko <Maliviwe.Diko@adaptit.com>
2022-12-14 14:38:24 +0200

    first commit

---
+++ Proj1_TextToMorseCodeConverter.py
@@ -0,0 +1,32 @@
+print("TEXT TO MORSE CODE CONVETER")
+print("Please input any string to convert into morse code")
+user_input = input("> ").upper()
+
+morse_dict = {'A': '.-', 'B': '-...',
+              'C': '-.-.', 'D': '-..', 'E': '.',
+              'F': '..-.', 'G': '--.', 'H': '....',
+              'I': '..', 'J': '.---', 'K': '-.-',
+              'L': '.-..', 'M': '--', 'N': '-.',
+              'O': '---', 'P': '.--.', 'Q': '--.-',
+              'R': '.-.', 'S': '...', 'T': '-',
+              'U': '..-', 'V': '...-', 'W': '.--',
+              'X': '-..-', 'Y': '-.--', 'Z': '--..',
+              '1': '.----', '2': '..---', '3': '...--',
+              '4': '....-', '5': '.....', '6': '-....',
+              '7': '--...', '8': '---..', '9': '----.',
+              '0': '-----', ', ': '--..--', '.': '.-.-.-',
+              '?': '..--..', '/': '-..-.', '-': '-....-',
+              '(': '-.--.', ')': '-.--.-'}
+
+
+def converter(message):
+    morse = []  # Will contain Morse versions of letters
+    for char in message:
+        if char in morse_dict:
+            morse.append(morse_dict[char])
+    return " ".join(morse)
+
+
+morse_code = converter(user_input)
+print("||||| Morse Code version below |||||")
+print(morse_code)
